4c2c78cf3ed28f5af0038d6feb8d62848a5774ad
safachaabane <safa1chaabane@gmail.com>
2021-05-21 12:35:34 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  state = {
+    Person : { fullName : "Safa Chaabane",bio: "i'm from Tunisia Bni Khalled", imgSrc:"/safsaf.jpg", profession:"i'm web developer "},
+    shows :false,
+    count : 0
+    
+  }
+  toggle = () =>{
+this.setState({Shows:!this.state.Shows})
 }
 
+  render() {
+    const count = this.state.count
+    return (
+      <div>
+         {
+          this.state.Shows === true ?
+          <div>
+          <h1>{this.state.Person.fullName}</h1>
+          <h1>{this.state.Person.profession}</h1>
+          <div><img src={this.state.Person.imgSrc} alt={"safa"}style={{width:500}}></img></div>
+          <h1>{this.state.Person.bio}</h1>
+          <h1>Current Count : {count}</h1>
+          <button onClick={()=>this.toggle()}>Hide</button>
+        </div>
+        : <div><h2>Show the counter </h2>
+        <button onClick={()=>this.toggle()}>Show</button></div>
+        }
+   
+
+         </div>)
+    }
+    componentDidMount() {
+      this.myInterval = setInterval (()=>{
+        this.setState(prevState => ({
+          count: prevState.count + 1
+      }))
+    }, 1000)
+    }
+
+    componentWillUnmount() {
+      clearInterval(this.myInterval)
+    }
+  }
 export default App;
---
+++ src/component/State.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class State extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default State
